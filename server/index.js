74263ff9a2e95d628952681e7bca24322c8f5da0
edmvl7 <edmondvalerie50@gmail.com>
2024-01-18 03:25:33 +0100

    update

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors')
+
 
 const authRoute = require('./routes/auth.route')
 
 dotenv.config();
 app.use(express.json())
+app.use(cors({origin: "http://localhost:5173", credentials:true})),
+
 app.use("/auth", authRoute)
 
 
