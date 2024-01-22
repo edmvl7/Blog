d7f0855f0e51330a704563d8a1bbd9f414a87c5c
edmvl7 <edmondvalerie50@gmail.com>
2024-01-22 14:18:49 +0100

    profil user

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import profilPic from '../assets/images/profile.jpg'
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+
+
+
+
 
 const Profile = () => {
   return (
-    <div>Profiljhjhkjke test trst</div>
+    <>
+      <div className='md:ms-6'>
+        <div className='m-8 border rounded-lg p-12'>
+          <div className='flex gap-8'> 
+            <img src={profilPic} alt="image" className="h-20 w-20 md:h-36 md:w-36 rounded-full bg-contain"/>
+            <div className='flex flex-col '>
+              <div className='flex justify-between'>
+                <h3 className='text-xl mb-2'>Name of user</h3>
+                <FaPencil/>
+              </div>
+              <p className='w-80'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel nisi illum cumque id quasi perferendis aliquid omnis harum dolorum error quisquam pariatur .</p>
+              <div className='flex mt-5 gap-5 '>
+                <FaXTwitter className='md:text-[21px] ' />
+                <FaInstagram className='md:text-[22px]' />
+                <FaTiktok className='md:text-[19px]' />
+              </div>
+            </div>
+          </div>
+         
+
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </>
   )
 }
 
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,27 +12,27 @@ import { CiSettings } from "react-icons/ci";
 const Sidebar = () => {
   return (
     <>
-        <div className=' md:flex md:flex-col md:w-56 fixed  bottom-0 left-0 right-0 md:top-0 md:h-full overflow-y-auto border-t-2 border-gray-50 md:border-e-2 '>
-            <div className=' flex justify-center md:flex-col  md:pt-20'>
-                <Link to={'/dashboard?tab=dash'}>           
-                    <div className='flex md:gap-4 p-6'>
+        <div className=' md:flex md:flex-col md:w-56 fixed mt-[70px] bottom-0 left-0 right-0 md:top-0 md:h-full overflow-y-auto border-t-2 border-gray-50 md:border-e-2 bg-gray-100 md:bg-transparent md:border-t-0 '>
+            <div className=' flex justify-center md:flex-col '>
+                <Link to={'/dashboard?tab=dash'} className=' md:border md:border-b-1 md:border-gray-200'>           
+                    <div className='flex md:gap-4 p-6 md:border-b-1 md:border-gray-200'>
                         <MdDashboard  className='m-1 text-2xl md:text-m' />
                         <p className='hidden md:block'>Dashboard</p>
                     </div>
                 </Link>
-                <Link to={'/dashboard?tab=dash'}>           
+                <Link to={'/dashboard?tab=dash'} className=' md:border md:border-b-1 md:border-gray-200'>           
                     <div className='flex gap-4 p-6'>
                         <FaUserAlt className='m-1 text-2xl md:text-m' />
                         <p className='hidden md:block'>Profile</p>
                     </div>
                 </Link>
-                <Link to={'/dashboard?tab=dash'}>           
+                <Link to={'/dashboard?tab=dash'} className=' md:border md:border-b-1 md:border-gray-200'>           
                     <div className='flex gap-4 p-6'>
                         <CiPen className='m-1 text-2xl md:text-m' />
                         <p className='hidden md:block'>Post</p>
                     </div>
                 </Link>
-                <Link to={'/dashboard?tab=dash'}>           
+                <Link to={'/dashboard?tab=dash'} className=' md:border md:border-b-1 md:border-gray-200'>           
                     <div className='flex gap-4 p-6'>
                         <CiSettings  className='m-1 text-2xl md:text-m' />
                         <p className='hidden md:block'>Dashboard</p>
